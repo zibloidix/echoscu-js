019dd73cdddc8345adbff219a4009a3115c5b267
zibloidix <zibloidix@gmail.com>
2020-01-28 15:58:06 +1100

    Fix code style - tabs to spaces

--- components/protocol-data-unit/protocol-data-unit.js
+++ components/protocol-data-unit/protocol-data-unit.js
@@ -1,17 +1,17 @@
 const pduTypes = [
-	require('../assoc-request'),
-	require('../assoc-accept'),
-	require('../assoc-reject'),
-	require('../data'),
-	require('../release-request'),
-	require('../release-response')
+  require('../assoc-request'),
+  require('../assoc-accept'),
+  require('../assoc-reject'),
+  require('../data'),
+  require('../release-request'),
+  require('../release-response')
 ];
 
 class ProtocolDataUnit {
-	constructor(buffer) {
-		debugger;
-		return pduTypes.map((pduType) => pduType.getInstance(buffer)).find((pduInstance) => pduInstance);
-	}
+  constructor(buffer) {
+    debugger;
+    return pduTypes.map((pduType) => pduType.getInstance(buffer)).find((pduInstance) => pduInstance);
+  }
 }
 
 module.exports = ProtocolDataUnit;

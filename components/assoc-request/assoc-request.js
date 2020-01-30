38d03a690b748a2dd5cc85ed4d705a96901807e7
zibloidix <zibloidix@gmail.com>
2020-01-30 16:01:33 +1100

    Add valueParseFunction

--- components/assoc-request/assoc-request.js
+++ components/assoc-request/assoc-request.js
@@ -7,14 +7,14 @@ class AssocRequest {
 		this.protocolVersion = this.getProtocolVersion();
 		this.calledEntryTitle = this.getCalledEntryTitle();
 		this.callingEntryTitle = this.getCallingEntryTitle();
-    this.applicationContext = this.getItem('10');
-    //this.presentationContext = this.getItem('20');
-    this.abstractSyntax = this.getItem('30');
-    this.transferSyntax = this.getItem('40');
+    this.applicationContext = this.getItem('10', this.parseValueToStr);
+    this.presentationContext = this.getItem('20', this.parseValueToInt, 1);
+    this.abstractSyntax = this.getItem('30', this.parseValueToStr);
+    this.transferSyntax = this.getItem('40', this.parseValueToStr);
     //this.userInfo = this.getItem('50');
-    this.maxPDULength =  this.getItem('51');
-    this.implementationUID = this.getItem('52');
-    this.implementationVersion = this.getItem('55');
+    this.maxPDULength =  this.getItem('51', this.parseValueToInt);
+    this.implementationUID = this.getItem('52', this.parseValueToStr);
+    this.implementationVersion = this.getItem('55', this.parseValueToStr);
   }
   
   getBuffer(buffer) {
@@ -40,21 +40,30 @@ class AssocRequest {
 		return this.buffer.header.toString('utf8', 26, 42).trim();
   }
   
-	getItem(itemCode) {
-		const itemTypePosition = this.buffer.body.indexOf(`${itemCode}00`, 'hex');
+	getItem(code, parseFunction, valueLength) {
+		const itemTypePosition = this.buffer.body.indexOf(`${code}00`, 'hex');
 		const itemLengthPosition = itemTypePosition + 2;
 		const itemValuePosition = itemLengthPosition + 2;
 
 		const itemType = this.buffer.body[itemTypePosition].toString(16);
-		const itemLength = this.buffer.body.readUIntBE(itemLengthPosition, 2);
-		const itemValue = this.buffer.body.toString('utf8', itemValuePosition, itemValuePosition + itemLength);
-		return {
+    const itemLength = this.buffer.body.readUIntBE(itemLengthPosition, 2);
+    const itemValueLength = valueLength ? itemValuePosition + valueLength : itemValuePosition + itemLength;
+		const itemValue = parseFunction(this.buffer.body.slice(itemValuePosition, itemValueLength));
+    
+    return {
 			itemType,
 			itemLength,
 			itemValue
 		};
+  }
+  
+  parseValueToStr(itemValue) {
+    return itemValue.toString('utf8');
+  }
 
-	}
+  parseValueToInt(itemValue) {
+    return itemValue.readUIntBE(0, itemValue.length);
+  }
 
 	static getInstance(buffer) {
 		const type = 0x01;
